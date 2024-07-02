273f02f5def96ca5a3d5d9fdb1fcd49cb1b4c9f6
Maiara Chagas <chagass.maiara@gmail.com>
2024-07-01 21:58:20 -0300

    feat: Exibindo sobre usuario

---
+++ lib/app/views/user/user_detail.dart
@@ -0,0 +1,201 @@
+import 'package:buscador_de_usuarios/app/controllers/repositories_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../controllers/users_controller.dart';
+
+class UserDetailPage extends StatefulWidget {
+  final String username;
+  const UserDetailPage({super.key, required this.username});
+
+  @override
+  State<UserDetailPage> createState() => _UserDetailPageState();
+}
+
+class _UserDetailPageState extends State<UserDetailPage> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<UsersController>(context, listen: false)
+        .getDetailUser(username: widget.username);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 15.0, vertical: 10.0),
+                child: Consumer<UsersController>(
+                  builder: (context, value, child) {
+                    switch (value.status) {
+                      case UserState.idle:
+                        return const Center();
+                      case UserState.loading:
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      case UserState.error:
+                        return Center(
+                          child: Text(value.errorMessage!),
+                        );
+                      default:
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 32,
+                                          child: ClipOval(
+                                            child: Image.network(
+                                              value.userDetail!.avatar,
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10),
+                                          child: Text(
+                                            value.userDetail!.username,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineMedium!
+                                                .copyWith(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 15, 0, 10),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "Repositórios: ${value.userDetail!.qtyRepos}",
+                                          ),
+                                          Text(
+                                            "Seguidores: ${value.userDetail!.qtyfollowers}",
+                                          ),
+                                          Text(
+                                            "Seguindo ${value.userDetail!.qtyfollowing}",
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const Divider(),
+                                    Consumer<RepositoriesController>(
+                                      builder: (context, repoValue, child) {
+                                        switch (repoValue.status) {
+                                          case RepositoriesState.idle:
+                                            return const Center();
+                                          case RepositoriesState.loading:
+                                            return const Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          case RepositoriesState.error:
+                                            return Center(
+                                              child:
+                                                  Text(repoValue.errorMessage!),
+                                            );
+                                          default:
+                                            return Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                const Text("Repositórios"),
+                                                ListView.builder(
+                                                    shrinkWrap: true,
+                                                    physics:
+                                                        const NeverScrollableScrollPhysics(),
+                                                    itemCount:
+                                                        repoValue.repos!.length,
+                                                    itemBuilder:
+                                                        (context, index) {
+                                                      return Container(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                .all(8),
+                                                        margin: const EdgeInsets
+                                                            .all(3),
+                                                        decoration:
+                                                            BoxDecoration(
+                                                          border: Border.all(
+                                                              width: 1),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(10),
+                                                        ),
+                                                        child: Column(
+                                                            mainAxisAlignment:
+                                                                MainAxisAlignment
+                                                                    .start,
+                                                            crossAxisAlignment:
+                                                                CrossAxisAlignment
+                                                                    .start,
+                                                            children: [
+                                                              Text(
+                                                                repoValue
+                                                                    .repos![
+                                                                        index]
+                                                                    .name,
+                                                                style: const TextStyle(
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .bold),
+                                                              ),
+                                                              Text(repoValue
+                                                                  .repos![index]
+                                                                  .language
+                                                                  .toLowerCase()),
+                                                            ]),
+                                                      );
+                                                    }),
+                                              ],
+                                            );
+                                        }
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
