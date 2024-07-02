19246739ab3654223d7455e3ec4bc19663b35b12
Maiara Chagas <chagass.maiara@gmail.com>
2024-07-01 21:57:11 -0300

    feat: Criando filtro para pesquisa

---
+++ lib/app/views/home/widgets/filters_widget.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class FilterWidget extends StatefulWidget {
+  final Function(Map<String, String>)? applyFilter;
+  final VoidCallback? clearFilter;
+
+  const FilterWidget({super.key, this.applyFilter, this.clearFilter});
+
+  @override
+  State<FilterWidget> createState() => _FilterWidgetState();
+}
+
+class _FilterWidgetState extends State<FilterWidget> {
+  final controllerSearch = TextEditingController();
+  final controllerLocation = TextEditingController();
+  final controllerLanguage = TextEditingController();
+  final controllerRepos = TextEditingController();
+  final controllerFollowers = TextEditingController();
+  String filter = "";
+  String txt = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 0),
+      child: Theme(
+        data: ThemeData(
+          dividerColor: Colors.transparent,
+        ),
+        child: ExpansionTile(
+          visualDensity: VisualDensity.compact,
+          tilePadding: EdgeInsets.zero,
+          childrenPadding: EdgeInsets.zero,
+          title: Text(
+            "FILTRAR POR",
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          children: [
+            _buildFilterField(
+                label: "Localidade",
+                controller: controllerLocation,
+                filterName: "location"),
+            const SizedBox(
+              height: 10,
+            ),
+            _buildFilterField(
+                label: "Linguagem de Programação",
+                controller: controllerLanguage,
+                filterName: "language"),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: _buildFilterField(
+                      label: "Nº de Seguidores",
+                      controller: controllerFollowers,
+                      filterName: "followers"),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: _buildFilterField(
+                      label: "Nº de Repositórios",
+                      controller: controllerRepos,
+                      filterName: "repos"),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        controllerFollowers.clear();
+                        controllerLanguage.clear();
+                        controllerLocation.clear();
+                        controllerRepos.clear();
+                        widget.clearFilter!();
+                      },
+                      child: const Text("Limpar filtro")),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                      onPressed: () {
+                        final filters = {
+                          'field': filter,
+                          'value': txt,
+                        };
+
+                        widget.applyFilter!(filters);
+                      },
+                      child: const Text("Aplicar filtro")),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFilterField({
+    required String label,
+    required TextEditingController controller,
+    required String filterName,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 3),
+          child: Text(label),
+        ),
+        SizedBox(
+          height: 45,
+          child: TextFormField(
+            controller: controller,
+            onChanged: (value) {
+              setState(() {
+                filter = filterName;
+                txt = value;
+              });
+            },
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
