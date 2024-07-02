49ef9adf47fc92b110c09229c02749f17db7738d
Maiara Chagas <61924605+maiarachagas@users.noreply.github.com>
2024-07-02 12:12:25 -0300

    feat: criado env.dart

---
+++ lib/app/env/env.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static const Map dataApi = {
+    "token":
+        "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",
+    "version": "",
+  };
+}
